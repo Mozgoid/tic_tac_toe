eb4295a2fe7e11cca6b663967dee69e1ac049dda
Sasha Shukala <mozgoid@gmail.com>
2023-08-27 11:44:53 +0200

    better cell position

--- Assets/Scripts/BoardView.cs
+++ Assets/Scripts/BoardView.cs
@@ -30,16 +30,17 @@ public class BoardView : MonoBehaviour
         cells = new CellView[board.Size, board.Size];
 
         var cellSize = cellPrefab.Size;
-        var cellBottomLeftStart = new Vector3(-cellSize.x * board.Size / 2, -cellSize.y * board.Size / 2, 0);
+        var cellBottomLeftStart = new Vector3(-cellSize.x * board.Size / 2.0f, -cellSize.y * board.Size / 2.0f, 0) + new Vector3(cellSize.x, cellSize.y) / 2.0f;
 
         Debug.Log(cellSize);
+        Debug.Log(cellBottomLeftStart);
 
         for (int i = 0; i < board.Size; i++)
         {
             for (int j = 0; j < board.Size; j++)
             {
                 var cell = Instantiate(cellPrefab, transform);
-                cell.transform.position = cellBottomLeftStart + new Vector3(cellSize.x * i, cellSize.y * j, 0);
+                cell.transform.localPosition = cellBottomLeftStart + new Vector3(cellSize.x * i, cellSize.y * j, 0);
                 cell.Position = new Vector2Int(i, j);
                 // cell.OnClick += OnCellClicked;
                 cells[i, j] = cell;
