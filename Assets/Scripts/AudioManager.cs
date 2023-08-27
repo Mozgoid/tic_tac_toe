decbbb818315b79d57d0b4dfc1ae1ab9730c71b8
Sasha Shukala <mozgoid@gmail.com>
2023-08-27 21:30:05 +0200

    project context and audio manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    private static float minVolume = -80;
+    private static float maxVolume = 0;
+
+    [System.Serializable]
+    public class MixerState
+    {
+        public AudioMixerGroup mixer;
+        public string SaveName => $"Volume{mixer.name}";
+
+        public void Load()
+        {
+            var savedValue = PlayerPrefs.GetFloat(SaveName, maxVolume);
+            var volume = Mathf.Lerp(minVolume, maxVolume, savedValue);
+            mixer.audioMixer.SetFloat($"{mixer.name}Volume", volume);
+        }
+
+        public void Set(float value)
+        {
+            PlayerPrefs.SetFloat(SaveName, value);
+            PlayerPrefs.Save();
+            var volume = Mathf.Lerp(minVolume, maxVolume, value);
+            mixer.audioMixer.SetFloat($"{mixer.name}Volume", volume);
+        }
+    }
+
+    [SerializeField] private MixerState sound, music;
+
+    public MixerState Sound => sound;
+    public MixerState Music => music;
+
+    private void Start()
+    {
+        sound.Load();
+        music.Load();
+    }
+
+}
---
+++ Assets/Scripts/SettingsView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsView : MonoBehaviour
+{
+
+    [SerializeField] private Slider sound, music;
+
+    [Zenject.Inject] private AudioManager audioManager;
+
+    private void Awake()
+    {
+        sound.onValueChanged.AddListener(audioManager.Sound.Set);
+        music.onValueChanged.AddListener(audioManager.Music.Set);
+    }
+
+    private void OnEnable()
+    {
+        sound.value = PlayerPrefs.GetFloat(audioManager.Sound.SaveName, 1);
+        music.value = PlayerPrefs.GetFloat(audioManager.Music.SaveName, 1);
+    }
+}
