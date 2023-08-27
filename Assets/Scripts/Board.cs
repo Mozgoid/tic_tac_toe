3cfdba4f75747a10422243cd69d2383729449e11
Sasha Shukala <mozgoid@gmail.com>
2023-08-27 09:48:11 +0200

    basic board and match

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board
+{
+    public enum Symbol
+    {
+        None,
+        X,
+        O,
+    }
+
+    public Symbol[,] board = new Symbol[3, 3];
+
+    public void Reset()
+    {
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++) 
+                board[i, j] = Symbol.None;
+        }
+    }
+
+    public Symbol WhoWins()
+    {
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            if (board[i, 0] != Symbol.None && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                return board[i, 0];
+        }
+
+        for (int j = 0; j < board.GetLength(1); j++)
+        {
+            if (board[0, j] != Symbol.None && board[0, j] == board[1, j] && board[1, j] == board[2, j])
+                return board[0, j];
+        }
+
+        if (board[0, 0] != Symbol.None && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            return board[0, 0];
+
+        if (board[0, 2] != Symbol.None && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            return board[0, 2];
+
+        return Symbol.None;
+    }
+}
---
+++ Assets/Scripts/Match.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match
+{
+    public Board.Symbol CurrentTurn { get; private set; }
+    public Board.Symbol Winner { get; private set; }
+}
