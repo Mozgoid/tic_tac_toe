641c1d5dc43f4e53c6be611ca7f7353f5fa83263
Sasha Shukala <mozgoid@gmail.com>
2023-08-27 23:47:22 +0200

    bundle editor

---
+++ Assets/Scripts/Editor/BundleEditor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class BundleEditor : EditorWindow
+{
+    string bundleName = "";
+    private Sprite X, O, Background;
+    private BuildTarget buildTarget = BuildTarget.StandaloneWindows;
+
+    
+    [MenuItem("Tools/BundleEditor")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(BundleEditor));
+    }
+    
+    void OnGUI()
+    {
+        bundleName = EditorGUILayout.TextField ("Bundle Name", bundleName);
+        
+        X = (Sprite)EditorGUILayout.ObjectField("X", X, typeof(Sprite), false);
+        O = (Sprite)EditorGUILayout.ObjectField("O", O, typeof(Sprite), false);
+        Background = (Sprite)EditorGUILayout.ObjectField("Background", Background, typeof(Sprite), false);
+        buildTarget = (BuildTarget)EditorGUILayout.EnumPopup("Build Target", buildTarget);
+
+        if (GUILayout.Button("Create Bundle"))
+        {
+            CreateBundle();
+        }
+    }
+
+    void CreateBundle()
+    {
+        if (string.IsNullOrEmpty(bundleName))
+        {
+            Debug.LogError("Bundle name is empty");
+            return;
+        }
+
+        if (X == null || O == null || Background == null)
+        {
+            Debug.LogError("Sprites are empty");
+            return;
+        }
+
+        var bundle = new AssetBundleBuild();
+        bundle.assetBundleName = bundleName;
+        bundle.assetNames = new string[]
+        {
+            AssetDatabase.GetAssetPath(X),
+            AssetDatabase.GetAssetPath(O),
+            AssetDatabase.GetAssetPath(Background),
+        };
+        BuildPipeline.BuildAssetBundles("Assets/StreamingAssets/", new AssetBundleBuild[] {bundle}, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
