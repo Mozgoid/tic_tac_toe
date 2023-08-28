7430d35e89ecd79deda71cb771fdc2bf0d47a098
Sasha Shukala <mozgoid@gmail.com>
2023-08-28 17:12:22 +0200

    status text

---
+++ Assets/Scripts/Match/StatusText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusText : MonoBehaviour
+{
+    [SerializeField] private TMPro.TextMeshProUGUI statusText;
+
+    [Zenject.Inject] private Match match;
+
+    private void Awake()
+    {
+        match.OnGameOver += OnGameOver;
+        match.OnPlayerSwitch += OnPlayerSwitch;
+    }
+
+    private void OnDestroy()
+    {
+        match.OnGameOver -= OnGameOver;
+        match.OnPlayerSwitch -= OnPlayerSwitch;
+    }
+
+    private void OnPlayerSwitch(Player player)
+    {
+        statusText.text = $"{player.Name} place {player.Symbol}";
+    }
+
+    private void OnGameOver(Board.Symbol winner)
+    {
+        if (winner == Board.Symbol.None)
+        {
+            statusText.text = "Draw";
+        }
+        else
+        {
+            statusText.text = $"{winner} wins";
+        }
+    }
+
+}
